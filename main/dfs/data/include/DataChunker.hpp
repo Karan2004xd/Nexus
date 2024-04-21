afee9952a2b644ada8f646023848837ab19f642e
Karan2004xd <prajapatikaran2004@gmail.com>
2024-04-22 00:27:08 +0530

    implemented data chunker

--- main/dfs/data/include/DataChunker.hpp
+++ main/dfs/data/include/DataChunker.hpp
@@ -1,22 +1,27 @@
 #ifndef DATA_CHUNKER_HPP
 #define DATA_CHUNKER_HPP
 
+#include <memory>
 #include <string>
 #include <vector>
-
-class Chunk;
+#include "Chunk.hpp"
 
 class DataChunker {
 protected:
   void chunkData(const std::string &, int);
+
 private:
   std::string MainData;
   int MainDataSize;
-  std::vector<Chunk> chunks;
+  std::vector<std::unique_ptr<Chunk>> chunks;
   
   double defaultChunkRatio;
+
+  size_t findChunkSize(const std::string &);
   void setDefaultChunkRatio(int);
   void setChunks();
-  std::vector<Chunk> getChunks();
+
+  std::vector<std::unique_ptr<Chunk>> getChunks();
+  void setMainData(const std::string &);
 };
 #endif // DATA_CHUNKER_HPP
--- main/dfs/data/src/DataChunker.cpp
+++ main/dfs/data/src/DataChunker.cpp
@@ -1,5 +1,5 @@
 #include "../include/DataChunker.hpp"
-#include "../include/Chunk.hpp"
+#include <cmath>
 
 void DataChunker::setDefaultChunkRatio(int MainDataSize) {
   this->MainDataSize = MainDataSize;
@@ -16,16 +16,42 @@ void DataChunker::setDefaultChunkRatio(int MainDataSize) {
     defaultChunkRatio = 1;
   }
 }
-void DataChunker::setChunks() {
-  int chunkNumber {1};
-  // Implement Chunking with encryption
+
+size_t findChunkSize(const std::string &chunk) {
+  size_t baseSize = sizeof(std::string);
+  size_t memoryAllocationSize = chunk.capacity() * sizeof(char);
+  return baseSize + memoryAllocationSize;
 }
 
-std::vector<Chunk> DataChunker::getChunks() {
+void DataChunker::setChunks() {
+  int chunkNumber {1};
+  int dataEndPos {0};
+  int ratio = ceil(MainDataSize * defaultChunkRatio);
+
+  while (dataEndPos < MainDataSize) {
+    int dataStartPos {dataEndPos};
+    if (dataEndPos + ratio > MainDataSize) {
+      dataEndPos = MainDataSize;
+    } else {
+      dataEndPos += ratio;
+    }
+    std::string chunk = MainData.substr(dataStartPos, dataEndPos - dataStartPos);
+    size_t chunkSize = findChunkSize(chunk);
+    chunks.push_back(std::make_unique<Chunk>(chunkNumber, chunkSize, chunk));
+  }
+}
+
+std::vector<std::unique_ptr<Chunk>> DataChunker::getChunks() {
   return this->chunks;
+}
+
+void DataChunker::setMainData(const std::string &mainData) {
+  this->MainData = mainData;
 }
 
 void DataChunker::chunkData(const std::string &mainData, int mainDataLength) {
   setDefaultChunkRatio(mainDataLength);
+  setMainData(mainData);
+  setChunks();
 }
 
